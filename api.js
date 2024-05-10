f5706bbe6dcd5219193563114df64d9ced722c31
bahayildirim <bahaberke12@gmail.com>
2024-05-10 13:38:28 +0300

    /envelopes endpoint

--- api.js
+++ api.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
-// Listener
+app.use(bodyParser.json());
+
+let envelopes = [];
+
 app.listen(PORT, (req, res) => {
     console.log(`Listening on port ${PORT}`);
 })
 
 app.get("/", (req, res) => {
     res.send("Hello, World");
+})
+
+app.post("/envelopes", (req, res) => {
+    const category = req.body.category;
+    const budget = req.body.budget;
+    if(category && budget) {
+        envelopes.push(req.body);
+        res.status(201).send("Envelope created");
+    } else {
+        res.status(400).send("Could not create envelope");
+    }
+})
+
+app.get("/envelopes", (req, res) => {
+    res.json(envelopes);
 })
